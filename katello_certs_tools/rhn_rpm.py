b3646e17f2512b66bf4e096bd918bba7bdb7b183
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-02-22 15:46:56 +0100

    Remove unused functions

--- katello_certs_tools/rhn_rpm.py
+++ katello_certs_tools/rhn_rpm.py
@@ -15,7 +15,6 @@
 
 from __future__ import print_function
 import os
-import sys
 import rpm
 import struct
 import functools
@@ -127,60 +126,6 @@ class RPM_Header:
             })
 
 
-def get_header_byte_range(package_file):
-    """
-    Return the start and end bytes of the rpm header object.
-
-    For details of the rpm file format, see:
-    http://www.rpm.org/max-rpm/s1-rpm-file-format-rpm-file-format.html
-    """
-
-    lead_size = 96
-
-    # Move past the rpm lead
-    package_file.seek(lead_size)
-
-    sig_size = get_header_struct_size(package_file)
-
-    # Now we can find the start of the actual header.
-    header_start = lead_size + sig_size
-
-    package_file.seek(header_start)
-
-    header_size = get_header_struct_size(package_file)
-
-    header_end = header_start + header_size
-
-    return (header_start, header_end)
-
-
-def get_header_struct_size(package_file):
-    """
-    Compute the size in bytes of the rpm header struct starting at the current
-    position in package_file.
-    """
-    # Move past the header preamble
-    package_file.seek(8, 1)
-
-    # Read the number of index entries
-    header_index = package_file.read(4)
-    (header_index_value, ) = struct.unpack('>I', header_index)
-
-    # Read the the size of the header data store
-    header_store = package_file.read(4)
-    (header_store_value, ) = struct.unpack('>I', header_store)
-
-    # The total size of the header. Each index entry is 16 bytes long.
-    header_size = 8 + 4 + 4 + header_index_value * 16 + header_store_value
-
-    # Headers end on an 8-byte boundary. Round out the extra data.
-    round_out = header_size % 8
-    if round_out != 0:
-        header_size = header_size + (8 - round_out)
-
-    return header_size
-
-
 SHARED_TS = None
 
 
@@ -229,60 +174,6 @@ def get_package_header(filename=None, file_stream=None, fd=None):
     return RPM_Header(hdr, is_source)
 
 
-class MatchIterator:
-    def __init__(self, tag_name=None, value=None):
-        # Query by name, by default
-        if not tag_name:
-            tag_name = "name"
-
-        # rpm 4.1 or later
-        self.ts = rpm.TransactionSet()
-        self.ts.setVSFlags(8)
-
-        m_args = (tag_name,)
-        if value:
-            m_args += (value,)
-        # pylint: disable=E1101
-        self.mi = self.ts.dbMatch(*m_args)
-
-    def pattern(self, tag_name, mode, pattern):
-        self.mi.pattern(tag_name, mode, pattern)
-
-    def next(self):
-        try:
-            hdr = self.mi.next()
-        except StopIteration:
-            hdr = None
-
-        if hdr is None:
-            return None
-        is_source = hdr[rpm.RPMTAG_SOURCEPACKAGE]
-        return RPM_Header(hdr, is_source)
-
-
-def headerLoad(data):
-    hdr = rpm.headerLoad(data)
-    is_source = hdr[rpm.RPMTAG_SOURCEPACKAGE]
-    return RPM_Header(hdr, is_source)
-
-
-def labelCompare(t1, t2):
-    return rpm.labelCompare(t1, t2)
-
-
-def nvre_compare(t1, t2):
-    def build_evr(p):
-        evr = [p[3], p[1], p[2]]
-        evr = map(str, evr)
-        if evr[0] == "":
-            evr[0] = None
-        return evr
-    if t1[0] != t2[0]:
-        raise ValueError("You should only compare packages with the same name")
-    evr1, evr2 = (build_evr(t1), build_evr(t2))
-    return rpm.labelCompare(evr1, evr2)
-
-
 def hdrLabelCompare(hdr1, hdr2):
     """ take two RPMs or headers and compare them for order """
 
@@ -319,21 +210,3 @@ def getInstalledHeader(rpmName):
     for h in mi:
         hdr = h
     return hdr
-
-
-if __name__ == '__main__':
-    app_mi = MatchIterator("name")
-    app_mi.pattern("name", rpm.RPMMIRE_GLOB, "*ker*")
-    while 1:
-        h = app_mi.next()
-        if not h:
-            break
-        print(h['name'])
-    sys.exit(1)
-    app_hdr1 = get_package_header(filename="/tmp/python-1.5.2-42.72.i386.rpm")
-    print(dir(app_hdr1))
-    # Sources
-    app_hdr1 = get_package_header(filename="/tmp/python-1.5.2-42.72.src.rpm")
-    app_hdr2 = headerLoad(app_hdr1.unload())
-    print(app_hdr2)
-    print(len(app_hdr2.keys()))
