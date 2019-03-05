1398fa7c637f34454af66cb9939e23e20df593c9
Evgeni Golov <evgeni@golov.de>
2019-03-05 09:38:58 +0100

    allow running tests inside a container
    this allows to run test.sh inside a container with smth like:
    
      podman run -v $(pwd):/app:Z --workdir=/app centos:7 bash .travis/test.sh
    
    it also adds tests that the generated RPM is actually installable and
    uncovers an issue when comparing the installed RPM to the generated one:
    
        ERROR: unhandled exception occurred:
        Traceback (most recent call last):
          File "/usr/bin/katello-ssl-tool", line 51, in <module>
            sys.exit(mod.main() or 0)
          File "/usr/lib/python2.7/site-packages/katello_certs_tools/katello_ssl_tool.py", line 1102, in main
            ret = _main() or 0
          File "/usr/lib/python2.7/site-packages/katello_certs_tools/katello_ssl_tool.py", line 1070, in _main
            genServerRpm(DEFS, options.verbose)
          File "/usr/lib/python2.7/site-packages/katello_certs_tools/katello_ssl_tool.py", line 893, in genServerRpm
            comp = hdrLabelCompare(h, hdr)
          File "/usr/lib/python2.7/site-packages/katello_certs_tools/rhn_rpm.py", line 292, in hdrLabelCompare
            if hdr1['name'] == hdr2['name']:
        TypeError: 'NoneType' object has no attribute '__getitem__'

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,13 +1,36 @@
-#!/bin/bash -e
+#!/bin/bash
+
+set -e
+
+export LC_ALL=en_US.UTF-8
+
+TEST_ON_EL=$([ -f /etc/redhat-release ] && [ -x /usr/bin/yum ] && echo "true" || echo "false")
+
+if [[ "${TEST_ON_EL}" == "true" ]]; then
+  yum install -y docbook-utils openssl rpm-build
+fi
+
+docbook2man katello-ssl-tool.sgml
+
+python setup.py install
 
 for i in `seq 1 4`; do
-katello-ssl-tool --gen-ca -p file:/etc/pki/katello/private/katello-default-ca.pwd --force --ca-cert-dir /etc/pki/katello-certs-tools/certs --set-common-name example.com --ca-cert katello-default-ca.crt --ca-key katello-default-ca.key --ca-cert-rpm katello-default-ca --set-country US --set-state "North Carolina" --set-city Raleigh --set-org Katello --set-org-unit SomeOrgUnit
+  katello-ssl-tool --gen-ca -p file:/etc/pki/katello/private/katello-default-ca.pwd --force --ca-cert-dir /etc/pki/katello-certs-tools/certs --set-common-name example.com --ca-cert katello-default-ca.crt --ca-key katello-default-ca.key --ca-cert-rpm katello-default-ca --set-country US --set-state "North Carolina" --set-city Raleigh --set-org Katello --set-org-unit SomeOrgUnit
+
+  if [[ "${TEST_ON_EL}" == "true" ]]; then
+    yum install -y $(ls -1t ./ssl-build/katello-default-ca-*.noarch.rpm|head -n1)
+  fi
+
 done
 
 for i in `seq 1 4`; do
-katello-ssl-tool --gen-server -p file:/etc/pki/katello/private/katello-default-ca.pwd --ca-cert-dir /etc/pki/katello-certs-tools/certs --set-common-name foo.example.com --ca-cert katello-default-ca.crt --ca-key katello-default-ca.key --set-country US --set-state "North Carolina" --set-city Raleigh --set-org Katello --set-org-unit SomeOrgUnit --cert-expiration 36500
+  katello-ssl-tool --gen-server -p file:/etc/pki/katello/private/katello-default-ca.pwd --ca-cert-dir /etc/pki/katello-certs-tools/certs --set-common-name foo.example.com --ca-cert katello-default-ca.crt --ca-key katello-default-ca.key --set-country US --set-state "North Carolina" --set-city Raleigh --set-org Katello --set-org-unit SomeOrgUnit --cert-expiration 36500
+
+  if [[ "${TEST_ON_EL}" == "true" ]]; then
+    yum install -y $(ls -1t ./ssl-build/$HOSTNAME/katello-httpd-ssl-key-pair-$HOSTNAME-*.noarch.rpm|head -n1)
+  fi
 done
 
 for i in `seq 1 4`; do
-katello-ssl-tool --gen-client -p file:/etc/pki/katello/private/katello-default-ca.pwd --ca-cert-dir /etc/pki/katello-certs-tools/certs --set-common-name foo.example.com --ca-cert katello-default-ca.crt --ca-key katello-default-ca.key --set-country US --set-state "North Carolina" --set-city Raleigh --set-org Katello --set-org-unit SomeOrgUnit --cert-expiration 36500
+  katello-ssl-tool --gen-client -p file:/etc/pki/katello/private/katello-default-ca.pwd --ca-cert-dir /etc/pki/katello-certs-tools/certs --set-common-name foo.example.com --ca-cert katello-default-ca.crt --ca-key katello-default-ca.key --set-country US --set-state "North Carolina" --set-city Raleigh --set-org Katello --set-org-unit SomeOrgUnit --cert-expiration 36500
 done
