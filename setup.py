dc85f2137db964a3e3ce84a9969753bf07f5acf6
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-09-13 18:57:39 +0200

    Release 2.6.0

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
 setup(name='Katello Certs Tools',
-      version='2.5.3',
+      version='2.6.0',
       description='Python modules used for Katello SSL tooling',
       author='Tomas Lestach',
       author_email='tlestach@redhat.com',
-      url='https://fedorahosted.org/katello/',
+      url='https://github.com/Katello/katello-certs-tools',
       packages=['katello_certs_tools'],
       scripts=['katello-ssl-tool', 'katello-sudo-ssl-tool', 'katello-certs-sign', 'katello-certs-gen-rpm'],
       data_files=[('share/man/man1', ['katello-ssl-tool.1'])])
