16a18c635b166bdf0f37a7427f9b4d06a9d099f3
Eric D. Helms <ericdhelms@gmail.com>
2021-06-01 20:17:07 -0400

    Release 2.8.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='Katello Certs Tools',
-      version='2.7.3',
+      version='2.8.0',
       description='Python modules used for Katello SSL tooling',
       author='Tomas Lestach',
       author_email='tlestach@redhat.com',
